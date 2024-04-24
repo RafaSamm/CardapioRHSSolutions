400d282eb891afca61e89fe181be8339ead39e65
RafaSamm <rafarhs286@gmail.com>
2024-04-24 12:11:05 -0300

    style: mais recursos e estilos considerados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"], //content = colocar as pastas e subpastas que utilizarão o
+    content: ["./**/*.{html,js}"], //content = colocar as pastas e subpastas que utilizarão
     theme: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+      },
       extend: {
         backgroundImage: { //Pegando a imagem de fundo da página
           "home": "url('/assets/bg.png')" // usando na tag HEADER por bg-home
